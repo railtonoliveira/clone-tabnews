760000718549c25935bcdea9838e5210707ada73
Railton Silva <72405356+railtonoliveira@users.noreply.github.com>
2024-10-09 20:38:44 +0000

    add `database.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,20 @@
+import { Client } from "pg";
+import { password } from "pg/lib/defaults";
+
+async function query(queryObjects) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObjects);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
